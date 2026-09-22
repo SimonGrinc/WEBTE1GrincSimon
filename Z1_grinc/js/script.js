549e314fadd0165feab34e1d489e9ad547b99418
SimonGrinc <xgrinc@stuba.sk>
2026-09-22 22:06:37 +0200

    Added a navbar most stuff still WIP but its getting there

---
+++ Z1_grinc/js/script.js
@@ -0,0 +1,6 @@
+var hamburger = document.querySelector('.hamburger');
+var navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', function () {
+  navLinks.classList.toggle('is-open');
+});
